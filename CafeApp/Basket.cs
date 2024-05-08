628ea969a557e9e588ed7feaacebb9ae1e691853
Blaise Mikka de Gier <ST10249838@vcconnect.edu.za>
2024-05-09 01:31:25 +0200

    BasketClass

---
+++ CafeApp/Basket.cs
@@ -0,0 +1,31 @@
+﻿namespace CafeApp
+{
+    // Item Basket class
+    class Basket
+    {
+        // Properties
+        private List<Item>? items;
+
+        // Constructor
+        public Basket() => items = new List<Item>();
+
+        // Add item to basket
+        public void AddItem(Item item) => items?.Add(item);
+
+        // Display items in basket
+        public string DisplayItems()
+        {
+            string display = "Name:\tQuantity\tPrice\n";
+            if (items != null)
+            {
+                foreach (Item item in items)
+                {
+                    display += $"{item.Name}\t{item.Quantity}\tR{item.Price}\n";
+                }
+                // Calculate total price
+                display += $"\nTotal Price: R{items.Sum(item => item.Price * item.Quantity)}";
+            }
+            return display;
+        }
+    }
+}
