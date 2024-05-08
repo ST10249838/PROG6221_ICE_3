ce8be26f0826e88937ac5ac69a87d05c335eda6d
Blaise Mikka de Gier <ST10249838@vcconnect.edu.za>
2024-05-09 01:30:15 +0200

    ItemClass

---
+++ CafeApp/Item.cs
@@ -0,0 +1,43 @@
+﻿namespace CafeApp
+{
+    //Item class
+    class Item
+    {
+        // Properties
+        private string name;
+        private int quantity;
+        private int price;
+
+        // Constructor
+        public Item(string name, int quantity)
+        {
+            if (name == "Espresso" || name == "Cappuccino" || name == "Latte" || name == "")
+            {
+                
+            }
+            this.name = name;
+            this.quantity = quantity;
+            switch (name)
+            {
+                case "Espresso":
+                    price = 20;
+                    break;
+                case "Cappuccino":
+                    price = 25;
+                    break;
+                case "Latte":
+                    price = 30;
+                    break;
+                case "Americano":
+                    price = 15;
+                    break;
+            }
+        }
+
+        // Getters and Setters
+        public string Name { get { return name; } }
+        public int Quantity { get { return quantity; } set { quantity = value; } }
+        public int Price { get { return price; } set { price = value; } }
+
+    }
+}
